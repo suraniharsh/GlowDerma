f3fe9637f71b1776dc59b00e7ac7b94c2f604942
suraniharsh <harshsurani00@gmail.com>
2025-01-24 10:30:45 +0530

    2203031080123 - Assignment 22

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to GlowDerma - Your Skincare Journey Begins Here");
-});
+app.use(express.json());
+
+const orders = [
+    { id: 1, product: 'Anti-Aging Serum', quantity: 2 },
+    { id: 2, product: 'Vitamin C Moisturizer', quantity: 1 },
+    { id: 3, product: 'Hyaluronic Acid', quantity: 3 }
+  ]
+
+const products = [
+  { id: 11, name: "Retinol Serum", price: 1200, availableQty: 50 },
+  { id: 12, name: "Niacinamide Solution", price: 800, availableQty: 30 },
+  { id: 14, name: "Peptide Moisturizer", price: 1500, availableQty: 100 },
+  { id: 15, name: "Glycolic Acid Toner", price: 900, availableQty: 20 }
+]
+
+const shoppingCart = [];
+
+  app.get("/", (req, res) => {
+    res.send("Welcome to GlowDerma - Your Skincare Journey Begins Here");
+  });
 
 app.get("/about", (req, res) => {
   res.send(`
@@ -40,6 +57,56 @@ app.get("/products", (req, res) => {
         "Brightens skin tone and reduces the appearance of dark spots.",
     },
   ]);
+});
+
+app.get("/orders/:orderID", (req, res) => {
+  const orderID = Number(req.params.orderID);
+  const order = orders.find((order) => order.id === orderID);
+
+  if (!order) {
+    res.status(404).json({ error: "Order Not Found" });
+  } else {
+    res.json(order);
+  }
+});
+
+app.get("/products", (req, res) => {
+  const name = req.query.name;
+  const maxPrice = Number(req.query.maxPrice);
+
+  if (name && maxPrice) {
+    const filteredProducts = products.filter(
+      (product) => product.name.includes(name) && product.price <= maxPrice
+    );
+    res.json(filteredProducts);
+  } else if (name) {
+    const filteredProducts = products.filter((product) =>
+      product.name.includes(name)
+    );
+    res.json(filteredProducts);
+  } else if (maxPrice) {
+    const filteredProducts = products.filter(
+      (product) => product.price <= maxPrice
+    );
+    res.json(filteredProducts);
+  } else {
+    res.json(products);
+  }
+});
+
+app.get("/cart", (req, res) => {
+  res.json(shoppingCart);
+});
+
+app.post("/cart", (req, res) => {
+  const { id, name, price, qty } = req.body;
+
+  if (!id || !name || !price || !qty) {
+    res.status(400).json({ error: "Missing fields" });
+  } else {
+    shoppingCart.push({ id, name, price, qty });
+    res.json(shoppingCart);
+  }
 });
 
 app.use((err, req, res, next) => {
